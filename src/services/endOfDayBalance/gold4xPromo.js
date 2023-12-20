b6f3886aefb899132af55f5a14f3ee7028f52ede
Enkhbayar Enkhorkhon <enkhbayar.e@motforex.com>
2023-12-20 15:07:25 +0800

    commit

--- src/services/endOfDayBalance/gold4xPromo.js
+++ src/services/endOfDayBalance/gold4xPromo.js
@@ -32,8 +32,8 @@ const getEndOfDay = async (groups, type) => {
         if (login.length > 3) {
           const finance = await getFinancial(
             login,
-            "2023-11-09 00:00:00",
-            "2023-12-16 23:59:59",
+            "2023-07-09 00:00:00",
+            "2023-12-31 23:59:59",
             100,
             MT5_SERVER_TYPE.LIVE
           );
@@ -42,7 +42,6 @@ const getEndOfDay = async (groups, type) => {
         }
       }
       generateExcell(list, `endOfDayBalanceGold4xPromo`);
-      // generateJson(list, `endOfDayBalanceGold4xPromo`);
     });
   } catch (error) {
     console.log(error);
@@ -88,12 +87,12 @@ function calculateEndOfDayBalances(resDeal, login, email) {
   let eodBalance = new Decimal(0);
   let eodWalletBalance = new Decimal(0);
   let eodAccountBalance = new Decimal(0);
+  let eodCreditBalance = new Decimal(0);
 
   resDeal.forEach((deal) => {
     const timestamp = parseInt(deal.Time, 10) * 1000;
     const date = new Date(timestamp);
     const dayKey = date.toISOString().split("T")[0];
-    // const dayKeyDate = date.toISOString().split("T")[0];
 
     if (!endOfDayBalances[dayKey]) {
       endOfDayBalances[dayKey] = {
@@ -102,6 +101,7 @@ function calculateEndOfDayBalances(resDeal, login, email) {
         email: email,
         profit: new Decimal(0),
         commission: new Decimal(0),
+        credit: new Decimal(0),
         deposit: new Decimal(0),
         withdraw: new Decimal(0),
         internalDeposit: new Decimal(0),
@@ -111,21 +111,31 @@ function calculateEndOfDayBalances(resDeal, login, email) {
         endOfDayAccountBalance: new Decimal(0),
         endOfDayWalletBalance: new Decimal(0),
         endOfDayBalanceTotal: new Decimal(0),
+        endOfDayCreditTotal: new Decimal(0),
         amount: new Decimal(0),
       };
     }
-    // endOfDayBalances[dayKey].date = dayKeyDate;
 
     const action = parseInt(deal.Action, 10);
     const profit = new Decimal(deal.Profit);
     const commission = new Decimal(deal.Commission);
     const comment = deal.Comment.toLowerCase() || "";
 
-    if (!profit.isZero()) {
-      endOfDayBalances[dayKey].amount =
-        endOfDayBalances[dayKey].amount.add(profit);
+    eodAccountBalance = eodAccountBalance.add(profit);
 
-      eodBalance = eodBalance.add(profit);
+    endOfDayBalances[dayKey].amount =
+      endOfDayBalances[dayKey].amount.add(profit);
+
+    if (!profit.isZero()) {
+      if (action == 0 || action == 1 || action == 2 || action == 5) {
+        eodBalance = eodBalance.add(profit);
+      }
+      if (action == 3) {
+        endOfDayBalances[dayKey].credit =
+          endOfDayBalances[dayKey].credit.add(profit);
+
+        eodCreditBalance = eodCreditBalance.add(profit);
+      }
 
       if (comment.includes("wallet")) {
         if (profit.greaterThan(0)) {
@@ -135,23 +145,16 @@ function calculateEndOfDayBalances(resDeal, login, email) {
           eodAccountBalance = eodAccountBalance.add(profit);
           eodWalletBalance = eodWalletBalance.sub(profit);
         }
-      } else {
-        eodAccountBalance = eodAccountBalance.add(profit);
       }
     }
 
     console.log(
-      `dayKey = ${dayKey} eodBalance = ${eodBalance}  action = ${action} profit = ${profit} commission = ${commission} comment = ${comment}`
+      `eod dayKey = ${dayKey} eodBalance = ${eodBalance}  action = ${action} profit = ${profit} commission = ${commission} comment = ${comment}`
     );
 
     if (action === 0 || action === 1) {
       endOfDayBalances[dayKey].profit =
         endOfDayBalances[dayKey].profit.add(profit);
-
-      if (!commission.isZero()) {
-        endOfDayBalances[dayKey].commission =
-          endOfDayBalances[dayKey].commission.add(commission);
-      }
 
       endOfDayBalances[dayKey].pnl = endOfDayBalances[dayKey].profit;
     } else if (action === 2) {
@@ -187,8 +190,7 @@ function calculateEndOfDayBalances(resDeal, login, email) {
     }
     endOfDayBalances[dayKey].endOfDayBalance = eodBalance;
     endOfDayBalances[dayKey].endOfDayAccountBalance = eodAccountBalance;
-    endOfDayBalances[dayKey].endOfDayWalletBalance = eodWalletBalance;
-    endOfDayBalances[dayKey].endOfDayBalanceTotal = eodBalance;
+    endOfDayBalances[dayKey].endOfDayCreditTotal = eodCreditBalance;
   });
 
   const endOfDayBalancesArray = Object.values(endOfDayBalances);
@@ -197,59 +199,51 @@ function calculateEndOfDayBalances(resDeal, login, email) {
 }
 
 function generateExcell(endOfDayBalances, path) {
-  //  Excel
   const workbook = new ExcelJS.Workbook();
   const worksheet = workbook.addWorksheet("Data");
 
-  // Define the headers for your Excel sheet
   worksheet.columns = [
     { header: "date", key: "date", width: 15 },
     { header: "login", key: "login", width: 15 },
-    // { header: "group", key: "group", width: 15 },
     { header: "email", key: "email", width: 15 },
-    // { header: "profit", key: "profit", width: 15 },
     { header: "commission", key: "commission", width: 15 },
+    { header: "credit", key: "credit", width: 15 },
     { header: "deposit", key: "deposit", width: 15 },
     { header: "withdraw", key: "withdraw", width: 15 },
     { header: "internalDeposit", key: "internalDeposit", width: 15 },
     { header: "internalWithdraw", key: "internalWithdraw", width: 15 },
     { header: "pnl", key: "pnl", width: 15 },
-    // { header: "endOfDayBalance", key: "endOfDayBalance", width: 15 },
+    { header: "endOfDayBalance", key: "endOfDayBalance", width: 15 },
     {
       header: "endOfDayAccountBalance",
       key: "endOfDayAccountBalance",
       width: 15,
     },
     {
-      header: "endOfDayWalletBalance",
-      key: "endOfDayWalletBalance",
+      header: "endOfDayCreditTotal",
+      key: "endOfDayCreditTotal",
       width: 15,
     },
-    // { header: "endOfDayBalanceTotal", key: "endOfDayBalanceTotal", width: 15 },
-    // { header: "amount", key: "amount", width: 15 },
+    { header: "amount", key: "amount", width: 15 },
   ];
 
-  // Add the data from the extractedData array to the worksheet
   endOfDayBalances.forEach((item) => {
-    // item.profit = parseFloat(item.profit);
     item.commission = parseFloat(item.commission);
+    item.credit = parseFloat(item.credit);
     item.deposit = parseFloat(item.deposit);
     item.withdraw = parseFloat(item.withdraw);
     item.internalDeposit = parseFloat(item.internalDeposit);
     item.internalWithdraw = parseFloat(item.internalWithdraw);
     item.pnl = parseFloat(item.pnl);
-    // item.endOfDayBalance = parseFloat(item.endOfDayBalance);
+    item.endOfDayBalance = parseFloat(item.endOfDayBalance);
     item.endOfDayAccountBalance = parseFloat(item.endOfDayAccountBalance);
-    item.endOfDayWalletBalance = parseFloat(item.endOfDayWalletBalance);
-    // item.endOfDayBalanceTotal = parseFloat(item.endOfDayBalanceTotal);
-    // item.amount = parseFloat(item.amount);
+    item.endOfDayCreditTotal = parseFloat(item.endOfDayCreditTotal);
+    item.amount = parseFloat(item.amount);
     worksheet.addRow(item);
   });
 
-  // Define the file path where you want to save the Excel file
   const filePath = `file/${path}.xlsx`;
 
-  // Save the Excel workbook to the file
   workbook.xlsx
     .writeFile(filePath)
     .then(() => {
